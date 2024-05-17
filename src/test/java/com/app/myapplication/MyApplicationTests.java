5105c6ae70784bb152b49ad2b67f98577c9b34d5
STALKSA <ksy9090@mail.ru>
2024-05-17 20:00:18 +0300

    added ci/cd

--- src/test/java/com/app/myapplication/MyApplicationTests.java
+++ src/test/java/com/app/myapplication/MyApplicationTests.java
@@ -1,13 +1,21 @@
 package com.app.myapplication;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MyApplicationTests {
+
+	@Autowired
+	private MyApplication context;
 
 	@Test
 	void contextLoads() {
+		assert(context != null);
+		System.out.println("Context loaded succussfully");
 	}
 
 }
